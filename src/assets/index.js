7de05bb627fd9e764946bd2307bdecd4503d50b0
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2024-12-29 23:25:50 +0530

    Fixed unwanted imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,36 +1,13 @@
-import airbnb from "./airbnb.png";
-import bill from "./bill.png";
-import binance from "./binance.png";
-import card from "./card.png";
-import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./codingClubLogo.png";
 import quotes from "./quotes.svg";
-import robot from "./back.png";
-import send from "./Send.svg";
-import shield from "./Shield.svg";
-import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
-import date from "./date.png"
 import location from './location.png'
-import problem from './problem.png'
-import download from './download.png'
-import arrow from './listArrow.png'
-import one from './1.png'
-import two from './2.png'
-import three from './3.png'
 import label1 from "./label1.webp"
 import label2 from "./label2.webp"
 import label3 from "./label3.webp"
@@ -74,37 +51,14 @@ export {
   label1,
   label2,
   label3,
-  three,
-  two,
-  one,
-  arrow,
-  download,
-  date,
   location,
-  problem,
-  airbnb,
-  bill,
-  binance,
-  card,
-  coinbase,
   dropbox,
-  logo,
   quotes,
-  robot,
-  send,
-  shield,
-  star,
   menu,
   close,
-  google,
-  apple,
   arrowUp,
-  discount,
   facebook,
   instagram,
   linkedin,
   twitter,
-  people01,
-  people02,
-  people03,
 };
